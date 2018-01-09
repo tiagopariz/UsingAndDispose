f32c23edb99a27ad61cbb8341ebdd012e3b34028
tiagopariz <tiagopariz@gmail.com>
2018-01-09 09:01:13 -0200

    Using and Dispose

---
+++ src/UsingAndDispose.Domain/TxtFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UsingAndDispose.Domain
+{
+    public class TxtFile : IDisposable
+    {
+        public TxtFile(string name, string folder)
+        {
+            Name = name;
+            Folder = folder;
+            FullPath = Folder + "/" + Name;
+        }
+
+        public string Name { get; }
+        public string Folder { get; }
+        public string FullPath { get; }
+
+        public void Close()
+        {
+            Console.WriteLine("Dispose object...");
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+    }
+}
---
+++ src/UsingAndDispose.Prompt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UsingAndDispose.Domain;
+
+namespace UsingAndDispose.Prompt
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var name = "Test";
+            var folder = "C:/Temp";
+
+            using (var file = new TxtFile(name, folder))
+            {
+                Console.WriteLine(file.FullPath);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
